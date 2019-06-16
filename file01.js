caf60bf27aaf8d8125b7f79fcff3d5aec80273e4
sumit saurabh <sumitsaurabh9868@gmail.com>
2019-06-16 23:51:31 +0530

    add 'nice to have'
    Signed-off-by: sumit saurabh <sumitsaurabh9868@gmail.com>

--- file01.js
+++ file01.js
@@ -2,3 +2,5 @@
 console.log('hello world')
 console.log('user "git status" to check status')
 console.log('use "git log" to see history')
+console.log('hello i did this one in')
+Console.log('nice to have you here')
