62dc064c1f8d28b4c210f311f0e21aaba8d88ced
sumit saurabh <sumitsaurabh9868@gmail.com>
2019-07-25 17:29:18 +0530

    trying to clean the tree

---
+++ jQuery/index.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function () {
+    alert('document loaded');
+    console.log('document loaded')
+});window.onload = function () {
+    alert('window loaded');
+    console.log('window loaded')
+};
